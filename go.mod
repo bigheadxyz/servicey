3f3af4ad43e86140b320ce0b2ed9bd6316e93abb
Amir So <amir.so@icloud.com>
2021-06-20 16:03:32 +0200

    X

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bigheadxyz/servicey
+
+go 1.16
+
+require github.com/bigheadxyz/hashgen v0.0.0-20210620135456-ff55f9df45e1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bigheadxyz/hashgen/pkg/hashcode"
+)
+
+func main() {
+	fmt.Println(hashcode.ReturnNumber())
+}
